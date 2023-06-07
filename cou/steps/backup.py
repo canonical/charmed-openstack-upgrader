c26eae30f07d36ccae3dc187ec810a7f89a03b92
Erhan Sunar <erhan.sunar@canonical.com>
2023-06-07 12:29:49 +0300

    Added backup functionality of database
    removed backup related code from zaza_utils

--- cou/steps/backup.py
+++ cou/steps/backup.py
@@ -15,8 +15,72 @@
 
 """Functions for backing up openstack database."""
 import logging
+import os
+from typing import Optional
+
+from cou.zaza_utils import model
+from cou.zaza_utils.upgrade_utils import (
+    extract_charm_name_from_url,
+    get_upgrade_candidates,
+)
 
 
 def backup() -> None:
     """Backup mysql database of openstack."""
     logging.info("Backing up mysql database")
+
+    mysql_app = get_database_app()
+    mysql_leader = model.get_unit_from_name(model.get_lead_unit_name(mysql_app))
+
+    logging.info("mysqldump mysql-innodb-cluster DBs ...")
+    action = model.run_action_on_leader(mysql_app, "mysqldump")
+    remote_file = action.data["results"]["mysqldump-file"]
+    basedir = action.data["parameters"]["basedir"]
+
+    logging.info("Set permissions to read mysql-innodb-cluster:%s ...", basedir)
+    model.run_on_leader(mysql_app, f"chmod o+rx {basedir}")
+
+    local_file = os.path.abspath(os.path.basename(remote_file))
+    logging.info("SCP from  mysql-innodb-cluster:%s to %s ...", remote_file, local_file)
+    model.scp_from_unit(mysql_leader.name, remote_file, local_file)
+
+    logging.info("Remove permissions to read mysql-innodb-cluster:%s ...", basedir)
+    model.run_on_leader(mysql_app, f"chmod o-rx {basedir}")
+
+
+def _check_db_relations(app_config: dict) -> bool:
+    """Check the db relations.
+
+    Gets the openstack database mysql-innodb-cluster application if there are more than one
+    application the one with the keystone relation is selected.
+
+    :param app_config: juju app config
+    :type app_config: str
+    :returns: True if it has a relation with keystone
+    :rtype: bool
+    """
+    for relation, app_list in app_config["relations"].items():
+        if relation == "db-router":
+            if len([a for a in app_list if "keystone".casefold() in a.casefold()]) > 0:
+                return True
+    return False
+
+
+def get_database_app(model_name: Optional[str] = None) -> Optional[str]:
+    """Get mysql-innodb-cluster application name.
+
+    Gets the openstack database mysql-innodb-cluster application if there are more than one
+    application the one with the keystone relation is selected.
+
+    :param model_name: Name of model to query.
+    :type model_name: str
+    :returns: Name of the mysql-innodb-cluster application name
+    :rtype: str
+    """
+    candidates = get_upgrade_candidates(model_name=model_name)
+    for app, app_config in candidates.items():
+        charm_name = extract_charm_name_from_url(app_config["charm"])
+        if charm_name == "mysql-innodb-cluster" and _check_db_relations(app_config):
+            return app
+
+    return None
--- tests/unit/steps/test_steps_backup.py
+++ tests/unit/steps/test_steps_backup.py
@@ -1,9 +1,66 @@
-from unittest import mock
+from unittest.mock import MagicMock, patch
 
-from cou.steps.backup import backup
+from cou.steps.backup import _check_db_relations, backup, get_database_app
 
 
-@mock.patch("cou.steps.backup.logging.info")
-def test_backup(log):
-    backup()
-    assert log.call_count == 1
+def test_backup():
+    with patch("cou.steps.backup.logging.info") as log, patch(
+        "cou.steps.backup.model"
+    ) as model, patch("cou.steps.backup.get_database_app"):
+        model.run_action_on_leader = MagicMock()
+        model.scp_from_unit = MagicMock()
+        model.get_unit_from_name = MagicMock()
+
+        backup()
+        assert log.call_count == 5
+
+
+def test_get_database_app():
+    with patch("cou.steps.backup.get_upgrade_candidates") as upgrade_candidates:
+        upgrade_candidates.return_value = {
+            "mysql": {
+                "charm": "mysql-innodb-cluster",
+                "series": "focal",
+                "os": "ubuntu",
+                "charm-origin": "charmhub",
+                "charm-name": "mysql-innodb-cluster",
+                "charm-rev": 43,
+                "charm-channel": "8.0/stable",
+                "charm-version": "13004be",
+                "can-upgrade-to": "ch:amd64/focal/mysql-innodb-cluster-56",
+                "relations": {
+                    "cluster": ["mysql"],
+                    "coordinator": ["mysql"],
+                    "db-router": [
+                        "cinder-mysql-router",
+                        "glance-mysql-router",
+                        "keystone-mysql-router",
+                        "neutron-api-mysql-router",
+                        "nova-cloud-controller-mysql-router",
+                        "openstack-dashboard-mysql-router",
+                        "placement-mysql-router",
+                    ],
+                },
+            }
+        }
+        app = get_database_app()
+        assert app == "mysql"
+
+
+def test_check_db_relations():
+    app_config = {
+        "relations": {
+            "cluster": ["mysql"],
+            "coordinator": ["mysql"],
+            "db-router": [
+                "cinder-mysql-router",
+                "glance-mysql-router",
+                "neutron-api-mysql-router",
+                "nova-cloud-controller-mysql-router",
+                "openstack-dashboard-mysql-router",
+                "placement-mysql-router",
+            ],
+        }
+    }
+    result = _check_db_relations(app_config)
+    assert not result
