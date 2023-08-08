0e2cbcf6f776a23fdcc17da6fd6a70659865d9e7
Erhan Sunar <erhan.sunar@canonical.com>
2023-08-08 20:22:01 +0300

    UI Fixes & License
    Update all files to conform with Apache-2.0 License.
    Fixed help message capitalization and enhance help messages.
    Default values for parameters are explicitly shown.

--- cou/cli.py
+++ cou/cli.py
@@ -1,17 +1,16 @@
-# Copyright 2023 Canonical Limited.
+# Copyright 2023 Canonical Limited
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Entrypoint to the 'charmed-openstack-upgrader'."""
 import argparse
@@ -45,9 +44,13 @@ def parse_args(args: Any) -> argparse.Namespace:
     :rtype: argparse.Namespace
     """
     parser = argparse.ArgumentParser(
-        description="description",
+        description="Charmed OpenStack Upgrader(cou) is an application to upgrade Charmed "
+        "OpenStack. Application supports:\n  - Focal/Ussuri -> Focal/Victoria "
+        "\n  - Focal/Victoria -> Focal/Xena\n  - Focal/Xena -> Focal/Wallaby\n  - Focal/Wallaby "
+        "-> Focal/Yoga \nupgrades.",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         exit_on_error=False,
+        add_help=False,
     )
     parser.add_argument(
         "--run",
@@ -61,16 +64,28 @@ def parse_args(args: Any) -> argparse.Namespace:
         dest="loglevel",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
         type=str.upper,
-        help="Set the logging level",
+        help="Set the logging level. Defaults to INFO. This only affects stdout. The logfile "
+        "will be always DEBUG. The file location is in COU_DATA/log. You can set the "
+        "COU_DATA via environment variable. However it needs to be plugged to snap.",
     )
     parser.add_argument(
         "--model",
         default=None,
         dest="model_name",
-        help="Set the model to operate on.",
+        help="Set the model to operate on. If not set it gets the model name in this order:\n"
+        "  1 - Environment variable JUJU_MODEL,"
+        "  2 - Environment variable MODEL_NAME,"
+        "  3 - Current active juju model",
     )
     parser.add_argument(
         "--non-interactive", help="Run upgrade without prompt.", action="store_true", default=False
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this help message and exit.",
     )
 
     return parser.parse_args(args)
