4732f60d2c72820e596ee772b5c08f05588e90f4
Erhan Sunar <esunar@users.noreply.github.com>
2023-08-11 00:53:58 +0300

    UI Fixes & License (#28)
    * UI Fixes & License
    
    Update all files to conform with Apache-2.0 License.
    Fixed help message capitalization and enhance help messages.
    Default values for parameters are explicitly shown.
    
    * Fixed help message, added scope and environment variables to README.md
    
    * Change wording
    
    Co-authored-by: Gabriel Cocenza <gabrielcocenza@gmail.com>
    
    * Fixed help message line length.

--- cou/utils/juju_utils.py
+++ cou/utils/juju_utils.py
@@ -1,5 +1,4 @@
-# mypy: disable-error-code="no-untyped-def"
-# Copyright 2018 Canonical Ltd.
+# Copyright 2023 Canonical Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Juju utilities for charmed-openstack-upgrader."""
 import logging
 import os
