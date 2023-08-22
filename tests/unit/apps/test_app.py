db0c8b0e2d9d053890e9a467346caadbd0698d55
Erhan Sunar <esunar@users.noreply.github.com>
2023-08-22 16:55:18 +0300

    Removed superfluous lines

--- tests/unit/apps/test_app.py
+++ tests/unit/apps/test_app.py
@@ -14,8 +14,6 @@
 
 import pytest
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from cou.apps.app import Application
 from cou.exceptions import MismatchedOpenStackVersions
 
