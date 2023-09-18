7a960c239c646b118c788082f51768c79dfb45d0
Tianqi <agileshaw@users.noreply.github.com>
2023-09-18 15:42:15 -0400

    Add unit tests

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -12,49 +12,172 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from argparse import ArgumentError
-from unittest.mock import MagicMock, patch
+import argparse
+from unittest.mock import AsyncMock, patch
 
 import pytest
 
-from cou.cli import entrypoint, parse_args, setup_logging
+from cou import cli
 from cou.exceptions import COUException, UnitNotFound
 from cou.steps import UpgradeStep
+from cou.steps.analyze import Analysis
 
 
 @pytest.mark.parametrize(
-    "args, expected_run, expected_loglevel, expected_interactive",
+    "verbosity_value, verbosity_name",
     [
-        (["--run", "--log-level", "DEBUG", "--no-interactive"], True, "DEBUG", False),
-        (["--run", "--log-level", "warning"], True, "WARNING", True),
-        (["--log-level", "debug"], False, "DEBUG", True),
+        (0, "ERROR"),
+        (1, "WARNING"),
+        (2, "INFO"),
+        (3, "DEBUG"),
+        (4, "NOTSET"),
+        (5, "NOTSET"),
+        (10, "NOTSET"),
     ],
 )
-def test_parse_args(args, expected_run, expected_loglevel, expected_interactive):
-    parsed_args = parse_args(args)
-    assert parsed_args.run == expected_run
-    assert parsed_args.loglevel == expected_loglevel
-    assert parsed_args.interactive == expected_interactive
+def test_verbosity_level(verbosity_value, verbosity_name):
+    """Test VerbosityLevel Enum class."""
+    level = cli.VerbosityLevel(verbosity_value)
+    assert level.name == verbosity_name
 
 
 @pytest.mark.parametrize(
-    "args, exception", [(["--log-level=DDD"], ArgumentError), (["--foo"], SystemExit)]
+    "verbosity_value, exception", [(-1, ValueError), ("UNEXPECTED", ValueError)]
 )
-def test_parse_args_raise_exception(args, exception):
+def test_verbosity_level_exception(verbosity_value, exception):
+    """Test VerbosityLevel Enum class with invalid inputs."""
     with pytest.raises(exception):
-        parse_args(args)
+        cli.VerbosityLevel(verbosity_value)
 
 
-def test_setup_logging():
-    with patch("cou.cli.logging") as mock_logging:
-        log_file_handler = MagicMock()
-        console_handler = MagicMock()
-        mock_root_logger = mock_logging.getLogger.return_value
-        mock_logging.FileHandler.return_value = log_file_handler
-        mock_logging.StreamHandler.return_value = console_handler
-        setup_logging("INFO")
-        mock_root_logger.addHandler.assert_any_call(log_file_handler)
-        mock_root_logger.addHandler.assert_any_call(console_handler)
+@pytest.mark.parametrize(
+    "quiet, verbosity, level",
+    [(True, 0, "CRITICAL"), (True, 5, "CRITICAL"), (False, 0, "ERROR"), (False, 2, "INFO")],
+)
+def test_get_log_level(quiet, verbosity, level):
+    """Test get_log_level return value."""
+    assert cli.get_log_level(quiet=quiet, verbosity=verbosity) == level
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "model_name, expected_call_count",
+    [("model_name", 0), ("", 1), (None, 1)],
+)
+async def test_analyze_and_plan(model_name, expected_call_count):
+    """Test analyze_and_plan function with different model_name arguments."""
+    analysis_result = Analysis(model_name=model_name, apps_control_plane=[], apps_data_plane=[])
+
+    with patch(
+        "cou.cli.juju_utils.get_current_model_name",
+        new=AsyncMock(return_value=model_name),
+    ) as mock_get_current_model_name, patch(
+        "cou.cli.generate_plan", new=AsyncMock()
+    ) as mock_generate_plan, patch(
+        "cou.cli.Analysis.create",
+        new=AsyncMock(return_value=analysis_result),
+    ) as mock_analyze:
+        await cli.analyze_and_plan(model_name)
+
+        assert mock_get_current_model_name.call_count == expected_call_count
+        mock_analyze.assert_awaited_once_with(model_name)
+        mock_generate_plan.assert_awaited_once_with(analysis_result)
+
+
+@pytest.mark.asyncio
+async def test_get_upgrade_plan():
+    """Test get_upgrade_plan function."""
+    plan = UpgradeStep(description="Top level plan", parallel=False, function=None)
+    plan.add_step(UpgradeStep(description="backup mysql databases", parallel=False, function=None))
+
+    with patch(
+        "cou.cli.analyze_and_plan", new=AsyncMock(return_value=plan)
+    ) as mock_analyze_and_plan, patch("cou.cli.logger") as mock_logger:
+        await cli.get_upgrade_plan()
+
+        mock_analyze_and_plan.assert_awaited_once()
+        mock_logger.info.assert_called_once_with(plan)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "quiet, expected_print_count",
+    [
+        (True, 1),
+        (False, 0),
+    ],
+)
+async def test_run_upgrade_quiet(quiet, expected_print_count):
+    """Test get_upgrade_plan function in either quiet or non-quiet mode."""
+    plan = UpgradeStep(description="Top level plan", parallel=False, function=None)
+    plan.add_step(UpgradeStep(description="backup mysql databases", parallel=False, function=None))
+
+    with patch(
+        "cou.cli.analyze_and_plan", new=AsyncMock(return_value=plan)
+    ) as mock_analyze_and_plan, patch("cou.cli.logger"), patch(
+        "cou.cli.apply_plan"
+    ) as mock_apply_plan, patch(
+        "builtins.print"
+    ) as mock_print:
+        await cli.run_upgrade(quiet=quiet)
+
+        mock_analyze_and_plan.assert_called_once()
+        mock_apply_plan.assert_called_once_with(plan, True)
+        mock_print.call_count == expected_print_count
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "interactive, progress_indication_count",
+    [
+        (False, 1),
+        (True, 0),
+    ],
+)
+async def test_run_upgrade_interactive(interactive, progress_indication_count):
+    """Test get_upgrade_plan function in either interactive or non-interactive mode."""
+    plan = UpgradeStep(description="Top level plan", parallel=False, function=None)
+    plan.add_step(UpgradeStep(description="backup mysql databases", parallel=False, function=None))
+
+    with patch(
+        "cou.cli.analyze_and_plan", new=AsyncMock(return_value=plan)
+    ) as mock_analyze_and_plan, patch("cou.cli.logger"), patch(
+        "cou.cli.apply_plan"
+    ) as mock_apply_plan, patch(
+        "cou.cli.progress_indicator"
+    ) as mock_progress_indicator:
+        await cli.run_upgrade(interactive=interactive)
+
+        mock_analyze_and_plan.assert_called_once()
+        mock_apply_plan.assert_called_once_with(plan, interactive)
+        assert mock_progress_indicator.start.call_count == progress_indication_count
+        assert mock_progress_indicator.succeed.call_count == progress_indication_count
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "command, function_call",
+    [
+        ("run", "cou.cli.run_upgrade"),
+        ("plan", "cou.cli.get_upgrade_plan"),
+    ],
+)
+async def test_entrypoint_commands(mocker, command, function_call):
+    """Test entrypoint with different commands."""
+    mocker.patch(
+        "cou.cli.parse_args",
+        return_value=argparse.Namespace(
+            quiet=False, command=command, verbosity=0, model_name=None, interactive=True
+        ),
+    )
+    mocker.patch("cou.cli.analyze_and_plan")
+    mocker.patch("cou.cli.setup_logging")
+    mocker.patch("cou.cli.apply_plan")
+    mocker.patch("cou.cli.Analysis.create")
+
+    with patch(function_call, new=AsyncMock()) as mock_function:
+        await cli.entrypoint()
+        mock_function.assert_awaited_once()
 
 
 @pytest.mark.asyncio
@@ -67,45 +190,13 @@ def test_setup_logging():
     ],
 )
 async def test_entrypoint_with_exception(mocker, exception, exp_exitcode):
-    mocker.patch("cou.cli.generate_plan", side_effect=exception)
-    mocker.patch("cou.cli.parse_args")
+    """Test entrypoint when exceptions are raised."""
+    mocker.patch(
+        "cou.cli.parse_args",
+        return_value=argparse.Namespace(quiet=False, command="plan", verbosity=0, model_name=None),
+    )
+    mocker.patch("cou.cli.analyze_and_plan", side_effect=exception)
     mocker.patch("cou.cli.setup_logging")
-    mocker.patch("cou.cli.apply_plan")
-    mocker.patch("cou.cli.Analysis.create")
 
     with pytest.raises(SystemExit, match=exp_exitcode):
-        await entrypoint()
-
-
-@pytest.mark.asyncio
-async def test_entrypoint_dry_run(mocker):
-    plan = UpgradeStep(description="Top level plan", parallel=False, function=None)
-    plan.add_step(UpgradeStep(description="backup mysql databases", parallel=False, function=None))
-
-    mock_parse_args = mocker.patch("cou.cli.parse_args")
-    mock_parse_args.return_value.run = False
-    mocker.patch("cou.cli.setup_logging")
-    mocker.patch("cou.cli.generate_plan", return_value=plan)
-    mock_apply_plan = mocker.patch("cou.cli.apply_plan")
-    mocker.patch("cou.cli.Analysis.create")
-    mock_print = mocker.patch("builtins.print")
-
-    result = await entrypoint()
-    mock_print.assert_called_with(plan)
-    mock_apply_plan.assert_not_called()
-
-    assert not result
-
-
-@pytest.mark.asyncio
-async def test_entrypoint_real_run():
-    with patch("cou.cli.parse_args") as mock_parse_args, patch("cou.cli.setup_logging"), patch(
-        "cou.cli.generate_plan"
-    ), patch("cou.cli.apply_plan") as mock_apply_plan, patch("cou.cli.Analysis.create"):
-        mock_parse_args.return_value = MagicMock()
-        mock_parse_args.return_value.run = True
-
-        result = await entrypoint()
-
-        assert not result
-        mock_apply_plan.assert_called_once()
+        await cli.entrypoint()
---
+++ tests/unit/test_commands.py
@@ -0,0 +1,361 @@
+# Copyright 2023 Canonical Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from argparse import ArgumentParser, Namespace
+from unittest.mock import patch
+
+import pytest
+
+from cou import commands
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        [],
+        ["-h"],
+        ["--help"],
+        ["help"],
+        ["help", "plan"],
+        ["help", "run"],
+        ["help", "all"],
+        ["plan", "-h"],
+        ["run", "-h"],
+        ["plan", "control-plane", "-h"],
+        ["plan", "data-plane", "-h"],
+        ["run", "control-plane", "-h"],
+        ["run", "data-plane", "-h"],
+    ],
+)
+def test_parse_args_help(args):
+    """Test printing help messages."""
+    with patch(
+        "cou.commands.argparse.ArgumentParser.print_help"
+    ) as mock_print_help, pytest.raises(SystemExit, match="0"):
+        commands.parse_args(args)
+        mock_print_help.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["--quiet", "--verbose"],
+        ["--quiet", "-v"],
+        ["--quiet", "-vvv"],
+    ],
+)
+def test_parse_args_quiet_verbose_exclusive(args):
+    """Test that quiet and verbose options are mutually exclusive."""
+    with pytest.raises(SystemExit, match="2"):
+        commands.parse_args(args)
+
+
+@pytest.mark.parametrize(
+    "args, expected_namespace",
+    [
+        (
+            ["plan"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                **{"upgrade-group": None}
+            ),
+        ),
+        (
+            ["plan", "--model=model_name"],
+            Namespace(
+                command="plan",
+                model_name="model_name",
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                **{"upgrade-group": None}
+            ),
+        ),
+        (
+            ["plan", "control-plane"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                **{"upgrade-group": "control-plane"}
+            ),
+        ),
+        (
+            ["plan", "data-plane"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                machines=None,
+                hostnames=None,
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["plan", "control-plane", "--verbose"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=1,
+                quiet=False,
+                **{"upgrade-group": "control-plane"}
+            ),
+        ),
+        (
+            ["plan", "data-plane", "--machine=1", "-m=2,3"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                machines=["1", "2,3"],
+                hostnames=None,
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["plan", "data-plane", "--quiet", "--availability-zone=1", "--az=2,3"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=True,
+                machines=None,
+                hostnames=None,
+                availability_zones=["1", "2,3"],
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["plan", "data-plane", "--parallel", "--hostname=1", "-n=2,3"],
+            Namespace(
+                command="plan",
+                model_name=None,
+                parallel=True,
+                verbosity=0,
+                quiet=False,
+                machines=None,
+                hostnames=["1", "2,3"],
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+    ],
+)
+def test_parse_args_plan(args, expected_namespace):
+    """Test parsing 'plan' subcommand and its arguments/options."""
+    parsed_args = commands.parse_args(args)
+
+    assert parsed_args == expected_namespace
+
+
+@pytest.mark.parametrize(
+    "args, expected_namespace",
+    [
+        (
+            ["run"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                **{"upgrade-group": None}
+            ),
+        ),
+        (
+            ["run", "--model=model_name"],
+            Namespace(
+                command="run",
+                model_name="model_name",
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                **{"upgrade-group": None}
+            ),
+        ),
+        (
+            ["run", "control-plane"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                **{"upgrade-group": "control-plane"}
+            ),
+        ),
+        (
+            ["run", "data-plane"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                machines=None,
+                hostnames=None,
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["run", "control-plane", "--verbose"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=1,
+                quiet=False,
+                interactive=True,
+                **{"upgrade-group": "control-plane"}
+            ),
+        ),
+        (
+            ["run", "data-plane", "--machine=1", "-m=2,3"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                machines=["1", "2,3"],
+                hostnames=None,
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["run", "data-plane", "--quiet", "--availability-zone=1", "--az=2,3"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=True,
+                interactive=True,
+                machines=None,
+                hostnames=None,
+                availability_zones=["1", "2,3"],
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["run", "data-plane", "--parallel", "--hostname=1", "-n=2,3"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=True,
+                verbosity=0,
+                quiet=False,
+                interactive=True,
+                machines=None,
+                hostnames=["1", "2,3"],
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+        (
+            ["run", "data-plane", "--no-interactive", "--hostname=1", "-n=2,3"],
+            Namespace(
+                command="run",
+                model_name=None,
+                parallel=False,
+                verbosity=0,
+                quiet=False,
+                interactive=False,
+                machines=None,
+                hostnames=["1", "2,3"],
+                availability_zones=None,
+                **{"upgrade-group": "data-plane"}
+            ),
+        ),
+    ],
+)
+def test_parse_args_run(args, expected_namespace):
+    """Test parsing 'run' subcommand and its arguments/options."""
+    parsed_args = commands.parse_args(args)
+
+    assert parsed_args == expected_namespace
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["run", "data-plane", "--machine 1", "--az 2"],
+        ["run", "data-plane", "--hostname 1", "-m 2"],
+        ["run", "data-plane", "--availability-zone 1", "-n 2"],
+        ["run", "data-plane", "--machine 1", "-n 2"],
+        ["run", "data-plane", "--az 1", "-m 2"],
+        ["run", "data-plane", "-m 1", "-n 2"],
+    ],
+)
+def test_parse_args_dataplane_exclusive_options(args):
+    """Test parsing mutually exclusive data-plane specific options."""
+    with pytest.raises(SystemExit, match="2"):
+        commands.parse_args(args)
+
+
+@pytest.mark.parametrize("args", [["foo"], ["--bar"]])
+def test_parse_args_raise_exception(args):
+    """Test parsing unknown arguments."""
+    with pytest.raises(SystemExit, match="2"):
+        commands.parse_args(args)
+
+
+def test_capitalize_usage_prefix():
+    """Test add usage with capitalized prefix."""
+    parser = ArgumentParser(formatter_class=commands.CapitalizeHelpFormatter)
+    usage = parser.format_usage()
+
+    assert usage.startswith("Usage: ")
+
+
+def test_capitalize_section_title():
+    """Test capitalizing option's section title."""
+    parser = ArgumentParser(formatter_class=commands.CapitalizeHelpFormatter)
+    help_text = parser.format_help()
+    section_title = ""
+    for line in help_text.splitlines():
+        if line.lower().startswith("options"):
+            section_title = line
+            break
+
+    assert section_title.startswith("Options")
+
+
+def test_capitalize_help_message():
+    """Test capitalizing help message."""
+    parser = ArgumentParser(formatter_class=commands.CapitalizeHelpFormatter)
+    help_message = parser.format_help()
+
+    assert "Show this help message and exit." in help_message
