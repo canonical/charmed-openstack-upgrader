59860891bfd67b0281f026a06d1d5c2e55acaca9
Tianqi <agileshaw@users.noreply.github.com>
2023-10-17 12:54:33 -0400

    Add additional compatible  OVN version in openstack_to_track.csv

--- cou/apps/auxiliary.py
+++ cou/apps/auxiliary.py
@@ -17,7 +17,6 @@ import logging
 from cou.apps.app import AppFactory, OpenStackApplication
 from cou.exceptions import ApplicationError
 from cou.utils.openstack import (
-    CHARM_FAMILIES,
     OPENSTACK_TO_TRACK_MAPPING,
     TRACK_TO_OPENSTACK_MAPPING,
     OpenStackRelease,
@@ -28,7 +27,6 @@ logger = logging.getLogger(__name__)
 
 @AppFactory.register_application(
     ["rabbitmq-server", "vault", "mysql-innodb-cluster", "ceph-fs", "ceph-radosgw"]
-    + CHARM_FAMILIES["ovn"]
 )
 class OpenStackAuxiliaryApplication(OpenStackApplication):
     """Application for charms that can have multiple OpenStack releases for a workload."""
