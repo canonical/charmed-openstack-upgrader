4aaf8d048b5154b60257ef0656e75c4225e60771
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2023-12-04 19:16:42 -0300

    - apply suggestions

--- tests/functional/tests/smoke.py
+++ tests/functional/tests/smoke.py
@@ -2,7 +2,7 @@ import logging
 import os
 import unittest
 from pathlib import Path
-from subprocess import STDOUT, CalledProcessError, check_call, check_output
+from subprocess import CalledProcessError, CompletedProcess, check_call, run
 
 import zaza
 
@@ -17,7 +17,9 @@ class SmokeTest(unittest.TestCase):
         zaza.get_or_create_libjuju_thread()
         cls.create_local_share_folder()
         cls.model_name = zaza.model.get_juju_model()
+        cls.package_installed = False
         cls.install_package()
+        cls.package_installed = True
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -33,10 +35,12 @@ class SmokeTest(unittest.TestCase):
         """Install cou package."""
         cou_test_snap = os.environ.get("TEST_SNAP")
         if cou_test_snap:
-            log.info(f"Installing {cou_test_snap}")
-            assert Path(cou_test_snap).is_file()
+            log.info("Installing %s", cou_test_snap)
+            assert Path(cou_test_snap).is_file(), f"{cou_test_snap} is not file"
             # install the snap
-            assert check_call(f"sudo snap install --dangerous {cou_test_snap}".split()) == 0
+            assert (
+                check_call(["sudo", "snap", "install", "--dangerous", cou_test_snap]) == 0
+            ), "cou snap installation failed"
             # connect interfaces
             interfaces_to_connect = [
                 "juju-client-observe",
@@ -44,33 +48,38 @@ class SmokeTest(unittest.TestCase):
                 "ssh-public-keys",
             ]
             for interface in interfaces_to_connect:
-                assert (
-                    check_call(
-                        f"sudo snap connect charmed-openstack-upgrader:{interface} snapd".split()
-                    )
-                    == 0
+                check_call(
+                    [
+                        "sudo",
+                        "snap",
+                        "connect",
+                        f"charmed-openstack-upgrader:{interface}",
+                        "snapd",
+                    ]
                 )
+
             # make the cou alias
-            assert check_call("sudo snap alias charmed-openstack-upgrader.cou cou".split()) == 0
+            check_call(["sudo", "snap", "alias", "charmed-openstack-upgrader.cou", "cou"])
 
             # check that the executable path exists
             assert Path("/snap/bin/cou").exists()
             cls.exc_path = "/snap/bin/cou"
 
         else:
-            # functest already installs cou as python package, but we install again
-            # for better developer experience
+            # functest already installs cou as python package
             log.warning("using cou as python package")
-            assert check_call(f"python3 -m pip install {os.getenv('PYTHONPATH')}".split()) == 0
-            cls.exc_path = "cou"
+            cls.exc_path = os.getenv("TEST_PYTHON_PACKAGE") + "/bin/cou"
 
-    def remove_snap_package() -> None:
+        log.info("Using cou path: %s", cls.exc_path)
+
+    @classmethod
+    def remove_snap_package(cls) -> None:
         """Remove cou package."""
-        if os.environ.get("TEST_SNAP"):
+        if os.environ.get("TEST_SNAP") and cls.package_installed:
             log.info("Removing snap package cou")
-            check_call("sudo snap remove charmed-openstack-upgrader --purge".split())
+            check_call(["sudo", "snap", "remove", "charmed-openstack-upgrader", "--purge"])
 
-    def cou(self, cmd: list[str]) -> str:
+    def cou(self, cmd: list[str]) -> CompletedProcess:
         """Run cou commands.
 
         :param cmd: Command to run.
@@ -79,13 +88,13 @@ class SmokeTest(unittest.TestCase):
         :rtype: str
         """
         try:
-            return check_output([self.exc_path] + cmd, stderr=STDOUT).decode()
+            return run([self.exc_path] + cmd, capture_output=True, text=True)
         except CalledProcessError as err:
             log.error(err.output)
 
-    def test_plan_backup(self) -> None:
+    def test_plan_default(self) -> None:
         """Test plan with backup."""
-        result = self.cou(["plan", "--model", self.model_name])
+        result = self.cou(["plan", "--model", self.model_name]).stdout
         expected_plan = (
             "Upgrade cloud from 'ussuri' to 'victoria'\n"
             "\tVerify that all OpenStack applications are in idle state\n"
@@ -102,22 +111,7 @@ class SmokeTest(unittest.TestCase):
 
     def test_plan_no_backup(self) -> None:
         """Test plan with no backup."""
-        result = self.cou(["plan", "--model", self.model_name, "--no-backup"])
-        expected_plan = (
-            "Upgrade cloud from 'ussuri' to 'victoria'\n"
-            "\tVerify that all OpenStack applications are in idle state\n"
-            "\tControl Plane principal(s) upgrade plan\n"
-            "\t\tUpgrade plan for 'designate-bind' to victoria\n"
-            "\t\t\tUpgrade software packages of 'designate-bind' "
-            "from the current APT repositories\n"
-            "\t\t\tUpgrade 'designate-bind' to the new channel: 'victoria/stable'\n"
-            "\t\t\tWait 300s for app designate-bind to reach the idle state.\n"
-            "\t\t\tCheck if the workload of 'designate-bind' has been upgraded\n"
-        )
-        self.assertIn(expected_plan, result)
-
-    def test_plan_no_backup_choosing_model(self) -> None:
-        result = self.cou(["plan", "--model", self.model_name, "--no-backup"])
+        result = self.cou(["plan", "--model", self.model_name, "--no-backup"]).stdout
         expected_plan = (
             "Upgrade cloud from 'ussuri' to 'victoria'\n"
             "\tVerify that all OpenStack applications are in idle state\n"
