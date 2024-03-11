5086374c97b1347196640728f8b57c852d68b5d6
Robert Gildein <gildeinrobert@gmail.com>
2024-03-11 09:17:34 +0100

    Add post-upgrade steps for whole upgrade plan (#279)
    Adding ceph-mon post-upgrade step to ensure require-osd-release option
    matches with ceph-osd version. Add also add_steps function to BaseStep
    so we can easily add more steps at once.
    
    ---------
    
    Co-authored-by: TQ X <agileshaw@users.noreply.github.com>

--- cou/apps/auxiliary.py
+++ cou/apps/auxiliary.py
@@ -195,7 +195,7 @@ class CephMon(AuxiliaryApplication):
         """
         ceph_mon_unit, *_ = self.units.values()
         return PreUpgradeStep(
-            description="Ensure require-osd-release option matches with ceph-osd version",
+            description="Ensure the 'require-osd-release' option matches the 'ceph-osd' version",
             coro=set_require_osd_release_option(ceph_mon_unit.name, self.model),
         )
 
