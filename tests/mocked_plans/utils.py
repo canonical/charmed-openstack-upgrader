0d83ca33771d68e2a0a4da7f046a34e30abb33fc
Robert Gildein <gildeinrobert@gmail.com>
2024-04-03 23:33:06 +0200

    Update tests/mocked_plans/utils.py
    Co-authored-by: TQ X <agileshaw@users.noreply.github.com>

--- tests/mocked_plans/utils.py
+++ tests/mocked_plans/utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Module to provide helper for writing unit tests."""
+"""Module to provide helper functions for writing mock upgrade tests."""
 from pathlib import Path
 from unittest.mock import AsyncMock, PropertyMock
 
