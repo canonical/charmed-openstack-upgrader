f2b2a8c045c8c12b68b1bafa63baecff8612d8c3
Ray Chan <chanchiwairay@gmail.com>
2024-06-24 09:56:14 +0800

    Update cou/steps/plan.py
    Co-authored-by: Samuel Allan <samuelengineer@fastmail.com>

--- cou/steps/plan.py
+++ cou/steps/plan.py
@@ -184,7 +184,7 @@ def _verify_highest_release_achieved(analysis_result: Analysis) -> None:
         raise HighestReleaseAchieved(
             f"No upgrades available for OpenStack {str(o7k_release).capitalize()} on "
             f"Ubuntu {current_series.capitalize()}.\nNewer OpenStack releases "
-            "is available after manually upgrading to a later Ubuntu series.\n"
+            "may available after manually upgrading to a later Ubuntu series.\n"
             "Charmed OpenStack Upgrader will not support upgrade across series,\n"
             "please refer to the official documentation on how to do series upgrade:\n"
             "- https://docs.openstack.org/charm-guide/latest/admin/upgrades/series.html\n"
